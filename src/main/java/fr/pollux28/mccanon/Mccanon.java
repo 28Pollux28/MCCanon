e820a8847e256810ac72f20feaba6b7af14cbc03
28Pollux28 <lemaire_valentin28@yahoo.fr>
2022-11-11 12:38:07 +0100

    init commit

---
+++ src/main/java/fr/pollux28/mccanon/Mccanon.java
@@ -0,0 +1,23 @@
+package fr.pollux28.mccanon;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.pollux28.mccanon.listener.MccanonListener;
+
+public final class Mccanon extends JavaPlugin {
+    public static final Logger logger = LoggerFactory.getLogger("Mccanon");
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/fr/pollux28/mccanon/listener/MccanonListener.java
@@ -0,0 +1,7 @@
+package fr.pollux28.mccanon.listener;
+
+import org.bukkit.event.Listener;
+
+public class MccanonListener implements Listener {
+
+}
